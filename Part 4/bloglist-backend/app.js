5450a479955e4b3aed8a38bc5ad41a9719340228
gabyNicliz <gabynicliz93@gmail.com>
2020-08-17 21:14:41 -0300

    deleted bloglist-frontend

--- Part 4/bloglist-backend/app.js
+++ Part 4/bloglist-backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
+
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
 const usersRouter = require('./controllers/users');
